135cd9241dc013a66a75714d263ac9fa41f17109
Scott Montague <SMontague29@gmail.com>
2017-06-18 21:46:40 -0600

    Update js

--- assets/js/work.js
+++ assets/js/work.js
@@ -1,1 +1,50 @@
 //JS for Gif Search here
+var topics = [];
+
+// Event listeners
+function addTopicClickEventListener() {
+	$(".topic").on("click", function() {
+
+		var topic = $(this).attr("data-name");
+
+		retrievegifs(topic);
+	});
+};
+
+function addgifClickEventListener() {
+	$(".gif").on("click", function() {
+
+	  var state = $(this).attr("data-state");
+
+	  if (state === "still") {
+	    $(this).attr("src", $(this).attr("data-animate"));
+	    $(this).attr("data-state", "animate");
+	  } else {
+	    $(this).attr("src", $(this).attr("data-still"));
+	    $(this).attr("data-state", "still");
+	  }
+	});
+};
+
+function addNewTopicClickEventListener() {
+	$("#add-animal").on("click", function() {
+
+	  event.preventDefault();
+
+	  var newTopic = $("#topic-input").val().trim();
+
+	  var alreadyTopicCheck = jQuery.inArray(newTopic, topics);
+
+	  if (newTopic === "") {
+	  	return;
+	  } else if (alreadyTopicCheck !== -1){
+	  	$("#topic-input").val("");
+	  	return;
+	  } else {
+
+	  	checkTopicExists(newTopic);
+
+	  }
+
+	});
+};
